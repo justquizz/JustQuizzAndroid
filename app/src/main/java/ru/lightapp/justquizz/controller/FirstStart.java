ff718f55a7f1ea1313f1ffd9fa69ed3fb5ae5f0d
jack <questpistols@gmail.com>
2016-03-14 22:22:31 +0300

    added activity FirstStart.java - 2

---
+++ app/src/main/java/ru/lightapp/justquizz/controller/FirstStart.java
@@ -0,0 +1,97 @@
+package ru.lightapp.justquizz.controller;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import ru.lightapp.justquizz.R;
+import ru.lightapp.justquizz.dataexchange.DBManager;
+import ru.lightapp.justquizz.dataexchange.FileManager;
+import ru.lightapp.justquizz.dataexchange.ServerManager;
+
+/**
+ * Created by eugen on 12.03.2016.
+ *
+ * Класс управляет активити первого запуска приложения first_start.xml
+ */
+public class FirstStart extends ActionBarActivity {
+
+    private ServerManager server;
+    private FileManager fileManager;
+    private DBManager db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_start);
+
+        server = ServerManager.getInstance();
+        fileManager = FileManager.getInstance();
+        db = DBManager.getInstance(this);
+    }
+
+
+
+    /*
+    * Обработка нажатия кнопок:
+    */
+    public void onClickDownloadAllDemo(View view) {
+
+        System.out.println(" --- onClickDownloadAllDemo");
+
+        ArrayList<String>[] tests  = server.getTestsByCategory(1);
+
+
+        //if(tests[5] != null) {
+            ArrayList<String> titleTestArray = tests[0];
+            ArrayList<String> fileNameArray = tests[1];
+            ArrayList<String> descriptionArray = tests[2];
+            //ArrayList<String> downloadsArray = tests[3];
+
+            /*
+            tests[0] = titleArray;
+            tests[1] = fileNameArray;
+            tests[2] = descriptionArray;
+            tests[3] = downloadsArray;
+            */
+
+
+            // TODO получили массив со всеми именами файлов. Теперь проходим в цикле: скачиваем и заносим в базу.
+
+            for (int i = 0; i <= titleTestArray.size() - 1; i++) {
+
+                if (fileManager.downloadTest(fileNameArray.get(i))) {
+
+                    String titleTest = titleTestArray.get(i);
+                    String fileNameTest = fileNameArray.get(i);
+                    String category = "1";
+                    String author = "Jack";
+                    String linkAuthorPage = "http://lightapp.ru/justquizz/people/id7";
+                    String descrTest = descriptionArray.get(i);
+
+
+                    long num = db.insertNewTest(titleTest, fileNameTest, category, author, linkAuthorPage, descrTest);
+
+                    if (num > 0) {
+                        Toast toast = Toast.makeText(getApplicationContext(), R.string.toast_download_success, Toast.LENGTH_SHORT);
+                        toast.show();
+
+                    }
+
+                    System.out.println(" --- файл скачан! - " + titleTest);
+                }
+            }
+        //}else{
+            //System.out.println(" --- tests == null" + tests[5].toString());
+        //}
+            db.resetFirstStart();
+    }
+
+
+}
